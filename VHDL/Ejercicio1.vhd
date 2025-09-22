b2698bf65bbe3fa074ebc9f8891a71f54e0fd03b
Alvorak <58303198+Alvorak@users.noreply.github.com>
2025-09-22 16:41:13 +0200

    Update Ejercicio1.vhd (resto de posibles soluciones lógicas añadidas)

--- VHDL/Ejercicio1.vhd
+++ VHDL/Ejercicio1.vhd
@@ -6,7 +6,7 @@ entity ejercicio1 is
     out Q IN std_logic); -- una sola salida
 end ejercicio1;
 
-architecture Behavior of ejercicio1 is
+architecture Behavior_if of ejercicio1 is
 begin
     process(A, B, C, S0, S1)
     begin
@@ -20,4 +20,35 @@ begin
                 end if;
         end if;
     end process;
-end Behavior;
+end Behavior_if;
+
+architecture Behavior_case of ejercicio1 is
+begin
+    process(A, B, C, S0, S1)
+    begin
+        case (S0 & S1) is  -- concatenamos los selectores para cogerlos en pares
+            when "00" => Q <= A;  -- S0=0 -> A
+            when "10" => Q <= B;  -- S0=1, S1=0 -> B
+            when "11" => Q <= C;  -- S0=1, S1=1 -> C
+            when others => Q <= '0';  -- caso "default" 
+        end case;
+    end process;
+end Behavior_case;
+
+
+architecture Behavior_select of ejercicio1 is
+begin
+    with (S0 & S1) select 
+        Q <= A when "00",
+             B when "10",
+             C when "11",
+             '0' when others; -- caso "default" 
+end Behavior_select;
+
+
+architecture Behavior_logico of ejercicio1 is
+begin
+    Q <= (A and (not S0)) or -- Q = A*S0' + B*S0*S1' + C*S0*S1 
+         (B and S0 and (not S1)) or
+         (C and S0 and S1);
+end Behavior_logico;
